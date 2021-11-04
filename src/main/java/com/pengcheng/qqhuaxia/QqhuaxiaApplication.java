779160296acfc98476f728228a8ee2ca50c7e698
z1026425556 <z53137473@126.com>
2021-11-04 13:14:10 +0800

    第一次提交代码

---
+++ src/main/java/com/pengcheng/qqhuaxia/QqhuaxiaApplication.java
@@ -0,0 +1,13 @@
+package com.pengcheng.qqhuaxia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqhuaxiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QqhuaxiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pengcheng/qqhuaxia/QqhuaxiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pengcheng.qqhuaxia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqhuaxiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
